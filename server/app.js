32512ad69c972bbe0f81b554eca14f3c7b392a35
John Miller <ctrjohnnie@gmail.com>
2018-05-15 20:27:22 -0600

    Added cors headers.

--- server/app.js
+++ server/app.js
@@ -7,12 +7,15 @@ var logger = require('morgan');
 var newsRouter = require('./routes/news');
 
 var app = express();
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/news', newsRouter);
 
